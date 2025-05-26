8e28e41c771606b15099265b5dcc16087a616aea
Benjamin Luzón Cáliz <bluzon@dome-consulting.com>
2025-05-26 20:27:00 +0200

    Añadir tablas a la db

---
+++ src/Entity/Campaign.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CampaignRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CampaignRepository::class)]
+class Campaign
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $start_date = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $end_date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeImmutable
+    {
+        return $this->start_date;
+    }
+
+    public function setStartDate(\DateTimeImmutable $start_date): static
+    {
+        $this->start_date = $start_date;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeImmutable
+    {
+        return $this->end_date;
+    }
+
+    public function setEndDate(\DateTimeImmutable $end_date): static
+    {
+        $this->end_date = $end_date;
+
+        return $this;
+    }
+}
